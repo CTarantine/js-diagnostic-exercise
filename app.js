42e97d09daf5d8da1ef77dcd0ce20f6b8e0f56a3
Chelsea Tarantine <asiajonesCB5@MacBook-Air-29.local>
2019-08-05 11:41:40 -0400

    listener added to checking

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello from app.js")
+const checkDeposit = document.querySelector("#depositC")
+
+//deposit click listener
+checkDeposit.addEventListener("click", (event) => {
+event.preventDefault();
+    console.log("Hello from app.js")
+})
+
+//javascript works
+document.body.style.backgroundColor = "red";
