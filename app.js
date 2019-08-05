0a4424a8dd58579f6a9016c00ff08d0ccb6d8862
Chelsea Tarantine <asiajonesCB5@MacBook-Air-29.local>
2019-08-05 11:50:50 -0400

    deposit click gets user input

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const checkDeposit = document.querySelector("#depositC")
-
+const checkInput = document.querySelector("#inputC")
 //deposit click listener
 checkDeposit.addEventListener("click", (event) => {
 event.preventDefault();
-    console.log("Hello from app.js")
+console.log(checkInput.value)
+    //console.log("Hello from app.js")
 })
 
 //javascript works
