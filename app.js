0c1b491ac543aae312439aaf79c02da9adf2cd9c
Chelsea Tarantine <asiajonesCB5@MacBook-Air-29.local>
2019-08-05 17:27:50 -0400

    color change according to balance

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-
+//deposit button for checking
 let checkDeposit = document.querySelector("#depositC");
 
 //deposit click listener
 checkDeposit.addEventListener("click", (event) => {
     event.preventDefault();
+    //grabs numeric value of typed input
     let checkInput = parseFloat(document.querySelector("#inputC").value);
+    //grabs numeric value of displayed balance
     let checkBalance = parseFloat(document.querySelector("#balC").innerHTML);
     let newBalance = checkInput + checkBalance;
     //console.log(newBalance);
@@ -12,15 +14,25 @@ checkDeposit.addEventListener("click", (event) => {
     //replace text in balance box w/ updated balance
     document.querySelector("#balC").innerHTML = newBalance
 })
-
+//withdraw button for checking
 let checkWithdraw = document.querySelector("#withdrawC")
+
 checkWithdraw.addEventListener("click", (event) => {
     event.preventDefault();
     let checkInput = parseFloat(document.querySelector("#inputC").value);
     let checkBalance = parseFloat(document.querySelector("#balC").innerHTML);
     let newBalance = checkBalance - checkInput;
 
-    document.querySelector("#balC").innerHTML = newBalance
+    if (newBalance === 0){
+        //display zero balance
+        document.querySelector("#balC").innerHTML = 0;
+        //change display
+        document.querySelector(".account").className = "zero";
+    }
+
+    if (newBalance > 0){
+        document.querySelector("#balC").innerHTML = newBalance
+    }
 })
 
 
