42f10dd8c9f63c1a90f0dc818a86673d77541f61
Chelsea Tarantine <asiajonesCB5@MacBook-Air-29.local>
2019-08-05 16:25:27 -0400

    added withdrawl functionality

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ let checkDeposit = document.querySelector("#depositC");
 //deposit click listener
 checkDeposit.addEventListener("click", (event) => {
     event.preventDefault();
-    let checkInput = parseInt(document.querySelector("#inputC").value);
-    let checkBalance = parseInt(document.querySelector("#balC").innerHTML);
+    let checkInput = parseFloat(document.querySelector("#inputC").value);
+    let checkBalance = parseFloat(document.querySelector("#balC").innerHTML);
     let newBalance = checkInput + checkBalance;
     //console.log(newBalance);
 
     //replace text in balance box w/ updated balance
+    document.querySelector("#balC").innerHTML = newBalance
+})
+
+let checkWithdraw = document.querySelector("#withdrawC")
+checkWithdraw.addEventListener("click", (event) => {
+    event.preventDefault();
+    let checkInput = parseFloat(document.querySelector("#inputC").value);
+    let checkBalance = parseFloat(document.querySelector("#balC").innerHTML);
+    let newBalance = checkBalance - checkInput;
+
     document.querySelector("#balC").innerHTML = newBalance
 })
 
